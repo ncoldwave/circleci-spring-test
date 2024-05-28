002f67e8265a01bf0b1c6f2b000b0f7445477eb1
ncoldwave <robbi.mobile@gmail.com>
2024-05-28 17:18:13 +0200

    Uploading Spring project

---
+++ src/main/java/com/example/circlecispringtest/CircleciSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.circlecispringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleciSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/circlecispringtest/CircleciSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circlecispringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
